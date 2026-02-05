473a85f37407de155ad1ebbc0e51801ca5d5ac11
Inano Knowles <inano_knowles@Inanos-MacBook-Pro-2.local>
2026-02-05 14:21:42 +1000

    updated

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -4,14 +4,14 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = '__all__'
+        fields = ("id", "username", "email", "password")
         extra_kwargs = {"password": {"write_only": True}}
-    
+
     def create(self, validated_data):
         user = CustomUser(
-            username=validated_data["username"],
-            email=validated_data["email"],
-            )
-        user.set_password(validated_data["password"])
+            username=validated_data.get("username"),
+            email=validated_data.get("email"),
+        )
+        user.set_password(validated_data.get("password"))
         user.save()
         return user
